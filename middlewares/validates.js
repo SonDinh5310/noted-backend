67072e3f3ca5de25a22e5cdeccea3b86e0ff8301
Sondinh <dnson20022001@gmail.com>
2022-06-26 11:14:42 +0700

    update login

--- middlewares/validates.js
+++ middlewares/validates.js
@@ -36,12 +36,8 @@ const loginValidate = (data) => {
             .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
             .required(),
 
-        password: Joi.string()
-            .pattern(
-                new RegExp(
-                    '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-.,]).{8,}$'
-                )
-            )
+        password:
+            Joi.string()
             .required(),
     });
 
